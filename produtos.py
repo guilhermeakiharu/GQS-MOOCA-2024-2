4e5af394d8a89bf7402d8fc897a54f7011695f7e
Sergio Eduardo Bossolani - 823150190 <823150190@ulife.com.br>
2024-11-11 11:40:45 -0300

    Correção de imprimir produto

--- produtos.py
+++ produtos.py
@@ -1,9 +1,9 @@
 # estado.py
 
-# Lista para armazenar os estados
+# Lista para armazenar os produtos
 produtos = []
 
-# Função para adicionar um estado
+# Função para adicionar um produto
 def adicionar_produto(nome_produto):
     if nome_produto not in produtos:
         produtos.append(nome_produto)
@@ -11,7 +11,7 @@ def adicionar_produto(nome_produto):
     else:
         print(f"O produto '{nome_produto}' já existe.")
 
-# Função para remover um estado
+# Função para remover um produto
 def remover_produto(nome_produto):
     if nome_produto in produtos:
         produtos.remove(nome_produto)
@@ -19,29 +19,29 @@ def remover_produto(nome_produto):
     else:
         print(f"O produto '{nome_produto}' não existe na lista.")
 
-# Função para contar o número de estados
+# Função para contar o número de produtos
 def contar_produtos():
-    return len(produtos)  # Retorna o total de estados
+    return len(produtos)  # Retorna o total de produtos
 
-# Função para imprimir todos os estados
+# Função para imprimir todos os produtos
 def imprimir_produtos():
     if produtos:
-        print("produtos:")
+        print("Produtos:")
         for produto in produtos:
             print(f"- {produto}")
     else:
-        print("Não há estados para exibir.")
+        print("Não há produtos para exibir.")
 
 # Exemplo de uso das funções
 if __name__ == "__main__":  # Isso só será executado se o script for executado diretamente
-    adicionar_produto("Bahia")
-    adicionar_produto("Minas Gerais")
-    adicionar_produto("São Paulo")
-    adicionar_produto("Recife")
-
-    adicionar_produto("Rio de Janeiro")
+    adicionar_produto("Broca")
+    adicionar_produto("Tijolo")
+    adicionar_produto("Porta")
+    adicionar_produto("Cimento")
+    adicionar_produto("Azulejo")
+    
     imprimir_produtos()
-    contar_produtos()
-    remover_produto("Bahia")
+    print(f"Total de produtos: {contar_produtos()}")
+    remover_produto("Broca")
     imprimir_produtos()
-    contar_produtos()
+    print(f"Total de produtos: {contar_produtos()}")
