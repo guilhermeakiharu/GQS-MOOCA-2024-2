d68d8fdbcfaf8dc5e006ffd179ac2b26688ce0f9
Guilherme Akiharu <84105586+guilhermeakiharu@users.noreply.github.com>
2024-11-11 10:27:15 -0300

    Update estados.py

--- estados.py
+++ estados.py
@@ -1,47 +1,47 @@
 # estado.py
 
 # Lista para armazenar os estados
-estados = []
+produtos = []
 
 # Função para adicionar um estado
-def adicionar_estado(nome_estado):
-    if nome_estado not in estados:
-        estados.append(nome_estado)
-        print(f"Estado '{nome_estado}' adicionado com sucesso.")
+def adicionar_produto(nome_produto):
+    if nome_produto not in produtos:
+        produtos.append(nome_produto)
+        print(f"Estado '{nome_produto}' adicionado com sucesso.")
     else:
-        print(f"O estado '{nome_estado}' já existe.")
+        print(f"O estado '{nome_produto}' já existe.")
 
 # Função para remover um estado
-def remover_estado(nome_estado):
-    if nome_estado in estados:
-        estados.remove(nome_estado)
-        print(f"Estado '{nome_estado}' removido com sucesso.")
+def remover_estado(nome_produto):
+    if nome_produto in produtos:
+        produtos.remove(nome_produto)
+        print(f"Estado '{nome_produto}' removido com sucesso.")
     else:
-        print(f"O estado '{nome_estado}' não existe na lista.")
+        print(f"O estado '{nome_produto}' não existe na lista.")
 
 # Função para contar o número de estados
-def contar_estados():
-    return len(estados)  # Retorna o total de estados
+def contar_produtos():
+    return len(produtos)  # Retorna o total de estados
 
 # Função para imprimir todos os estados
-def imprimir_estados():
-    if estados:
-        print("Estados:")
-        for estado in estados:
-            print(f"- {estado}")
+def imprimir_produtos():
+    if produtos:
+        print("produtos:")
+        for produto in produtos:
+            print(f"- {produto}")
     else:
         print("Não há estados para exibir.")
 
 # Exemplo de uso das funções
 if __name__ == "__main__":  # Isso só será executado se o script for executado diretamente
-    adicionar_estado("Bahia")
-    adicionar_estado("Minas Gerais")
-    adicionar_estado("São Paulo")
-    adicionar_estado("Recife")
+    adicionar_produto("Bahia")
+    adicionar_produto("Minas Gerais")
+    adicionar_produto("São Paulo")
+    adicionar_produto("Recife")
 
-    adicionar_estado("Rio de Janeiro")
-    imprimir_estados()
-    contar_estados()
-    remover_estado("Bahia")
-    imprimir_estados()
-    contar_estados()
+    adicionar_produto("Rio de Janeiro")
+    imprimir_produtos()
+    contar_produtos()
+    remover_produto("Bahia")
+    imprimir_produtos()
+    contar_produtos()
